8c59ac365eb80105626af44ca7fe2c9152b070a5
vladimirmaciel <vladimirmaciel@gmail.com>
2021-11-14 02:57:24 -0300

    implementado função de atualizar envetns

--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -7,6 +7,14 @@ from .models import Event, Venue
 from .forms import VenueForm, EventForm
 
 
+def update_event(request, event_id):
+    event = Event.objects.get(pk=event_id) # passado o id do objeto desejado
+    form = EventForm(request.POST or None, instance=event)
+    if form.is_valid():
+        form.save()
+        return redirect('list-events')
+    return render(request, 'events/update_event.html',{'event':event, 'form':form})
+
 
 def add_event(request):
 	submitted = False
